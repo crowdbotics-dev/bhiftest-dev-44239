d6bbcaa71aa4b4669e892f5121721ea426594735
Crowdbotics <engineering@crowdbotics.com>
2022-11-14 05:44:24 +0000

    Initial commit.

---
+++ backend/bhiftest_dev_44239/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for bhiftest_dev_44239 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bhiftest_dev_44239.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "bhiftest-dev-44239",
+  url: "https://bhiftest-dev-44239.botics.co",
+  api: "https://bhiftest-dev-44239.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
